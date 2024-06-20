f393517b2938b6ee802e9f4f9c09fc0997661de1
hungnguyen26 <142713851+hungnguyen26@users.noreply.github.com>
2024-06-20 23:46:03 +0700

    Kết nối db

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const database = require("./config/database");
 const app = express();
 const port = 3000;
+
+database.connect();     // kết nối db
 
 app.get("/tasks",(req,res)=>{
     res.send("Danh sách công việc");
