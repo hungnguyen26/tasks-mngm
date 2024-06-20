85eef675917de783b19e5a43f1a07c0abb01a4eb
hungnguyen26 <142713851+hungnguyen26@users.noreply.github.com>
2024-06-21 00:13:26 +0700

    Viết API lấy ra chi tiết công việc

--- index.js
+++ index.js
@@ -4,17 +4,33 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT;
 
-database.connect();     // kết nối db
+database.connect(); // kết nối db
 
 const Task = require("./model/tasks.model");
 
-app.get("/tasks",async (req,res)=>{
-    const tasks = await Task.find({
-        deleted:false,
-    })
-    res.json(tasks);
-})
+app.get("/tasks", async (req, res) => {
+  const tasks = await Task.find({
+    deleted: false,
+  });
+  res.json(tasks);
+});
 
-app.listen(port,()=>{
-    console.log(`App listening on port ${port}`);
-})
+app.get("/tasks/detail/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const task = await Task.findOne({
+      _id: id,
+      deleted: false,
+    });
+
+    res.json(task);
+  } catch (error) {
+    res.json("Khong tim thay")
+  }
+
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
