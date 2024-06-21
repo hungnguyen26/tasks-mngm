3b35a9754e202bf486125b1c540642c0a5b474d5
hungnguyen26 <142713851+hungnguyen26@users.noreply.github.com>
2024-06-21 23:01:57 +0700

    Cấu hình theo mô hình MVC

--- api/v1/router/task.router.js
+++ api/v1/router/task.router.js
@@ -1,30 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-const Task = require("../../../model/tasks.model");
 
-// const controller = require("../../controllers/client/cart.controller");
+const controller = require("../controllers/tasks.controller");
 
-router.get("/", async (req, res) => {
-  const tasks = await Task.find({
-    deleted: false,
-  });
-  res.json(tasks);
-});
+router.get("/", controller.index);
 
-router.get("/detail/:id", async (req, res) => {
-  try {
-    const id = req.params.id;
-
-    const task = await Task.findOne({
-      _id: id,
-      deleted: false,
-    });
-
-    res.json(task);
-  } catch (error) {
-    res.json("Khong tim thay");
-  }
-});
+router.get("/detail/:id", controller.detail);
 
 module.exports = router;
