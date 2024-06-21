22c0980c2a8ff5efe2ee5a57b42ef9b9d28cbdcb
hungnguyen26 <142713851+hungnguyen26@users.noreply.github.com>
2024-06-21 23:25:25 +0700

    Bộ lọc theo trạng thái

--- api/v1/controllers/tasks.controller.js
+++ api/v1/controllers/tasks.controller.js
@@ -2,10 +2,14 @@ const Task = require("../model/tasks.model");
 
 // [GET] /api/v1/tasks
 module.exports.index = async (req, res) => {
-  const tasks = await Task.find({
-    deleted: false,
-  });
-  res.json(tasks);
+    const find = {
+        deleted: false,
+    }
+    if(req.query.status){
+        find.status = req.query.status;
+    }
+    const tasks = await Task.find(find);
+    res.json(tasks);
 };
 
 // [GET] /api/v1/tasks/detail/:id
