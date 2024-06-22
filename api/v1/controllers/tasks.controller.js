6633c2233a8c63822ca082204c61bde9b91f1429
hungnguyen26 <142713851+hungnguyen26@users.noreply.github.com>
2024-06-22 23:19:26 +0700

    API phân trang

--- api/v1/controllers/tasks.controller.js
+++ api/v1/controllers/tasks.controller.js
@@ -1,24 +1,40 @@
 const Task = require("../model/tasks.model");
+const paginationHelper = require("../../../helper/pagination");
 
 // [GET] /api/v1/tasks
 module.exports.index = async (req, res) => {
-    const find = {
-        deleted: false,
-    }
-    
-    if(req.query.status){
-        find.status = req.query.status;
-    }
-    // console.log(req.query);
+  const find = {
+    deleted: false,
+  };
 
-    // sort
-    const sort = {};
-    if(req.query.sortKey && req.query.sortValue){
-        sort[req.query.sortKey] = req.query.sortValue;
-    }
-    // end sort
-    const tasks = await Task.find(find).sort(sort);
-    res.json(tasks);
+  if (req.query.status) {
+    find.status = req.query.status;
+  }
+  // console.log(req.query);
+
+  // phần phân trang
+  const countTasks = await Task.countDocuments(find); // đếm tổng số sản phẩm
+  let objectPagination = paginationHelper(
+    {
+      limitItem: 2, // sl phần tử mỗi trang
+      currentPage: 1,
+    },
+    req.query,
+    countTasks
+  );
+  // end phần phân trang
+
+  // sort
+  const sort = {};
+  if (req.query.sortKey && req.query.sortValue) {
+    sort[req.query.sortKey] = req.query.sortValue;
+  }
+  // end sort
+  const tasks = await Task.find(find)
+    .sort(sort)
+    .limit(objectPagination.limitItem)
+    .skip(objectPagination.skip);
+  res.json(tasks);
 };
 
 // [GET] /api/v1/tasks/detail/:id
