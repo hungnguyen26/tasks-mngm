53adc2a39457b1c21f4e073eafdcec00e24ff0aa
hungnguyen26 <142713851+hungnguyen26@users.noreply.github.com>
2024-06-23 23:22:40 +0700

    Tính năng đăng nhập

--- api/v1/router/users.router.js
+++ api/v1/router/users.router.js
@@ -6,4 +6,6 @@ const controller = require("../controllers/users.controller");
 
 router.post("/register", controller.register);
 
+router.post("/login", controller.login);
+
 module.exports = router;
